762884af42f862eefa21ecf53967670d56352055
graviscool <88552913+graviscool@users.noreply.github.com>
2024-07-10 13:09:51 -0700

    wrap whole page in container to allow dark mode customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { Container } from "react-bootstrap";
 import TransferOptions from "./components/TransferOptions";
 
 export default function Home() {
   return (
     <>
-      <h1 className="header-title text-center mb-3 mt-3">
-        Cheap PSU Transfer Credit
-      </h1>
-      <TransferOptions />
+      <Container fluid>
+        <h1 className="header-title text-center mb-3 mt-3">
+          Cheap PSU Transfer Credit
+        </h1>
+        <TransferOptions />
+      </Container>
     </>
   );
 }

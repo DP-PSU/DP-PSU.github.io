bf27a1113cb6cd067517abfacfe9b64fd379438a
graviscool <88552913+graviscool@users.noreply.github.com>
2024-07-10 18:39:14 -0700

    metadata + change allowed origins bc of domain change

--- src/app/api/reviews/getreviews/route.tsx
+++ src/app/api/reviews/getreviews/route.tsx
@@ -1,9 +1,6 @@
 import { MongoClient } from "mongodb";
 import { NextResponse } from "next/server";
-const allowedOrigins = [
-  "https://dp-psu-next.vercel.app",
-  "http://localhost:3000",
-];
+const allowedOrigins = ["https://dp-psu.vercel.app", "http://localhost:3000"];
 
 export async function POST(req: Request) {
   if (!allowedOrigins.includes(req.headers.get("origin")!))
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,14 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Cheap PSU Transfer Credit",
   description:
     "Cheap Transfer Credit for PSU students (ranging from free courses to courses that cost less than 500 bucks). These credit sources include Sophia Learning, Thomas Edison University, CLEP, DSST, Study.com, Arizona State University, and American Dream Academy.",
+  keywords: ["transfer", "transfer credit", "cheap", "penn state", "psu"],
+  
 };
 
 export default function RootLayout({

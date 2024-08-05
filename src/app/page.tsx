8a2b0d6c7da740c0709e0d13dc92004fc0dd4e44
graviscool <88552913+graviscool@users.noreply.github.com>
2024-08-04 22:46:39 -0700

    fix: suspense boundary for searchparams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,32 @@
 import Container from "react-bootstrap/Container";
 import TransferOptions from "./components/TransferOptions";
 import NavigationBar from "./components/NavigationBar";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import { useSearchParams } from "next/navigation";
 import { Toast, ToastContainer } from "react-bootstrap";
 
+function SearchParams({
+  showNewWebsiteDialog,
+}: Readonly<{
+  showNewWebsiteDialog: () => void;
+}>) {
+  const params = useSearchParams();
+  useEffect(() => {
+    if (
+      params.get("src") == "old_url" &&
+      !localStorage.getItem("newWebsiteDialogShown")
+    ) {
+      showNewWebsiteDialog();
+    }
+  }, [params, showNewWebsiteDialog]);
+
+  return <></>;
+}
+
 export default function Home() {
   const [darkState, setDarkState] = useState(true);
-  const params = useSearchParams();
+
   const [showNewWebsiteToast, setShowNewWebsiteToast] = useState(false);
 
   useEffect(() => {
@@ -20,44 +38,40 @@ export default function Home() {
     }
   }, []);
 
-  useEffect(() => {
-    if (
-      params.get("src") == "old_url" &&
-      !localStorage.getItem("newWebsiteDialogShown")
-    ) {
-      showNewWebsiteDialog();
-    }
-  }, [params]);
-
   const showNewWebsiteDialog = () => {
     setShowNewWebsiteToast(true);
     localStorage.setItem("newWebsiteDialogShown", JSON.stringify(true));
   };
 
   return (
-    <Container fluid className={`p-0 ${darkState ? "bg-secondary" : ""}`}>
-      <NavigationBar dark={darkState} setDark={setDarkState} />
-      <TransferOptions dark={darkState} />
-      <Footer dark={darkState} />
-      <ToastContainer position="top-end">
-        <Toast
-          className="mt-5"
-          show={showNewWebsiteToast}
-          onClose={() => setShowNewWebsiteToast(false)}
-          bg="info"
-          autohide
-          delay={7500}
-        >
-          <Toast.Header>
-            <strong className="me-auto">Welcome to the new website!</strong>
-            <small>now</small>
-          </Toast.Header>
-          <Toast.Body>
-            You can now view and post reviews based on your experience with the
-            various options!
-          </Toast.Body>
-        </Toast>
-      </ToastContainer>
-    </Container>
+    <>
+      <Container fluid className={`p-0 ${darkState ? "bg-secondary" : ""}`}>
+        <NavigationBar dark={darkState} setDark={setDarkState} />
+        <TransferOptions dark={darkState} />
+        <Footer dark={darkState} />
+        <ToastContainer position="top-end">
+          <Toast
+            className="mt-5"
+            show={showNewWebsiteToast}
+            onClose={() => setShowNewWebsiteToast(false)}
+            bg="info"
+            autohide
+            delay={7500}
+          >
+            <Toast.Header>
+              <strong className="me-auto">Welcome to the new website!</strong>
+              <small>now</small>
+            </Toast.Header>
+            <Toast.Body>
+              You can now view and post reviews based on your experience with
+              the various options!
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
+      </Container>
+      <Suspense>
+        <SearchParams showNewWebsiteDialog={showNewWebsiteDialog} />
+      </Suspense>
+    </>
   );
 }
